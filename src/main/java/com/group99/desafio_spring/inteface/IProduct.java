71242cc5a7fa9c53359a49ce376995d8080e5736
ranisantos <raniel.santos@mercadolivre.com>
2022-10-05 15:25:04 -0300

    Adiciona metodos para ordenar em ordem alfabética e parametros de filtro

--- src/main/java/com/group99/desafio_spring/inteface/IProduct.java
+++ src/main/java/com/group99/desafio_spring/inteface/IProduct.java
@@ -1,9 +1,13 @@
 package com.group99.desafio_spring.inteface;
 
 import com.group99.desafio_spring.dto.ProductDTO;
+import com.group99.desafio_spring.model.Product;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IProduct {
     List<ProductDTO> getAll();
+
+    List<ProductDTO> getFiltered(Optional<String> category, Optional<Boolean> freeShipping, Optional<Integer> order, Optional<String> prestige);
 }
