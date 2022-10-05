c6ef6cc4e07ab729acca737074fbab7ec918988a
Vitor Oliveira Silva <vitor.soliveira@mercadolivre.com>
2022-10-05 11:03:34 -0300

    feat: Product Controller

---
+++ src/main/java/com/group99/desafio_spring/controller/ProductController.java
@@ -0,0 +1,25 @@
+package com.group99.desafio_spring.controller;
+
+import com.group99.desafio_spring.inteface.IProduct;
+import com.group99.desafio_spring.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/articles")
+public class ProductController {
+
+    @Autowired
+    private IProduct service;
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAll(){
+        return new ResponseEntity<>(service.getAll(), HttpStatus.OK);
+    }
+}
