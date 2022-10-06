eaf7923f8b5b3e90f013360e5f4e008a83b12472
Vitor Oliveira Silva <vitor.soliveira@mercadolivre.com>
2022-10-06 11:07:45 -0300

    feat: addClient method logic

--- src/main/java/com/group99/desafio_spring/repository/ClientRepo.java
+++ src/main/java/com/group99/desafio_spring/repository/ClientRepo.java
@@ -4,11 +4,11 @@ import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.group99.desafio_spring.model.Client;
-import com.group99.desafio_spring.model.Product;
 import org.springframework.stereotype.Repository;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Repository
@@ -17,13 +17,26 @@ public class ClientRepo {
 
     ObjectMapper mapper = new ObjectMapper();
 
+    public List<Client> getAll(){
+        List<Client> clients = null;
+
+        try {
+            clients = Arrays.asList(mapper.readValue(new File(pathFile), Client[].class));
+        } catch (Exception ex) {
+
+        }
+        return clients;
+    }
     public void addClient(Client client) {
         ObjectWriter writer = mapper.writer(new DefaultPrettyPrinter());
 
-        List<Client> = new ArrayList<>()
+        List<Client> clients = this.getAll();
+
+        clients = new ArrayList<>(clients);
+        clients.add(client);
 
         try {
-            writer.writeValue(new File(pathFile), products);
+            writer.writeValue(new File(pathFile), clients);
         } catch (Exception ex) {
 
         }
